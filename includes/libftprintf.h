3c6a61643a904da9d3706129ca1748457c55affd
CleSucre <julien.thomas.4321@gmail.com>
2022-12-07 19:11:10 +0100

    update

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jthomas <jthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 03:45:00 by jthomas           #+#    #+#             */
-/*   Updated: 2022/12/07 17:36:46 by jthomas          ###   ########.fr       */
+/*   Updated: 2022/12/07 17:50:30 by jthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int ft_printf(const char *format, ...);
-int	ft_isflag(char c);
-void    ft_printflag(char c, va_list *args);
+int		ft_printf(const char *format, ...);
+int		ft_isflag(char c);
+void	ft_printflag(char c, va_list *args);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_base_fd(int nbr, char *base, int fd);
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jthomas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 23:46:38 by jthomas           #+#    #+#             */
-/*   Updated: 2022/12/07 17:20:29 by jthomas          ###   ########.fr       */
+/*   Updated: 2022/12/07 18:05:19 by jthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ int	ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] != '%')
-			write(1, &format[i++], 1);
+		{
+			ft_putchar_fd(format[i++], 1);
+			count++;
+		}
 		else if (ft_isflag(format[++i]))
 		{
 			ft_printflag(format[i++], &args);
-			count++;
 		}
 	}
 	va_end(args);
