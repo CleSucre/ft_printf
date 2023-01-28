dec8ec255c23eaee09fc1343ab7ed51b17d18137
CleSucre <julien.thomas.4321@gmail.com>
2023-01-28 15:26:58 +0100

    fix norminette

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jthomas <jthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 03:45:00 by jthomas           #+#    #+#             */
-/*   Updated: 2022/12/08 02:46:39 by jthomas          ###   ########.fr       */
+/*   Updated: 2023/01/28 15:25:57 by jthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int		ft_printf(const char *format, ...);
-int 	ft_printflag(int fd, const char *format, va_list args);
-int		ft_puthex_fd(int n, char format, int fd);
-int		ft_putunsigned_fd(unsigned int n, int fd);
+int	ft_printf(const char *format, ...);
+int	ft_printflag(int fd, const char *format, va_list args);
+int	ft_puthex_fd(int n, char format, int fd);
+int	ft_putunsigned_fd(unsigned int n, int fd);
 #endif
