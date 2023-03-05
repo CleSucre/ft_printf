cdb45edce79e12110b01876fa7ff6e6880cd1583
CleSucre <julien.thomas.4321@gmail.com>
2023-03-05 07:26:13 +0100

    Update CMakeLists.txt

--- main.c
+++ main.c
@@ -11,18 +11,9 @@
 /* ************************************************************************** */
 
 #include "includes/libftprintf.h"
-#include <stdio.h>
 
-int	main(void)
-{
-	void	*p;
-	int		ret1;
-	int		ret2;
+int main(void) {
 
-	p = (void *)0x7fff5fbffb90;
-	ft_putstr_fd("===============TESTS START HERE===============\n", 1);
-	ret1 = ft_printf("%p\n", p);
-	ret2 = printf("%p\n", p);
-	printf("ft_printf: %d, printf: %d\n", ret1, ret2);
+	ft_printf("Hello World! My age is %d", 17);
 	return (0);
 }
